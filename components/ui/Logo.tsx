54b92494cc83d9ec3db4e002efc37f37409e5ae5
Cristian Omar Jiménez Sánchez <cristianomar71@outlook.com>
2025-10-21 13:19:30 -0600

    Se maquetan las vistas de iniciar sesion, crear cuenta y reestablecer contraseña

---
+++ components/ui/Logo.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image"
+
+export default function Logo() {
+  return (
+    <Image src="/logo.svg" alt="Logo CashTrackr" width={400} height={100} priority className="w-full" />
+  )
+}
