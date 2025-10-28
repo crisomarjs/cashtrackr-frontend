d7aea25fea9e9581c4b0a2c16c7a40d11b75fc2e
Cristian Omar Jiménez Sánchez <cristianomar71@outlook.com>
2025-10-28 15:06:48 -0600

    Se grafica el procentaje del total gastado

--- app/admin/budget/[id]/page.tsx
+++ app/admin/budget/[id]/page.tsx
@@ -1,5 +1,7 @@
+import ProgressBar from '@/components/budgets/ProgressBar'
 import AddExpenseButton from '@/components/expenses/AddExpenseButton'
 import ExpenseMenu from '@/components/expenses/ExpenseMenu'
+import Amount from '@/components/ui/Amount'
 import ModalContainer from '@/components/ui/ModalContainer'
 import { getBudget } from '@/src/services/budgets'
 import { formatCurrency, formatDate } from '@/src/utils'
@@ -21,6 +23,11 @@ export default async function BudgetDetailsPage({ params }: { params: Promise<{ 
     const { id } = await params
     const budget = await getBudget(id)
 
+    const totalSpent = budget.expenses.reduce((total, expense) => +expense.amount + total, 0)
+    const totalAvailable = +budget.amount - totalSpent
+
+    const percentage = +((totalSpent / +budget.amount) * 100).toFixed(2)
+
     return (
         <>
             <div className='flex justify-between items-center'>
@@ -34,6 +41,29 @@ export default async function BudgetDetailsPage({ params }: { params: Promise<{ 
 
             {budget.expenses.length ? (
                 <>
+
+                    <div className='grid grid-cols-1 md:grid-cols-2 mt-10'>
+                        <div>
+                            <ProgressBar
+                                percentage={percentage}
+                            />
+                        </div>
+                        <div className='flex flex-col justify-center items-center md:items-start gap-5'>
+                            <Amount
+                                label="Presupuesto"
+                                amount={+budget.amount}
+                            />
+                            <Amount
+                                label="Disponible"
+                                amount={totalAvailable}
+                            />
+                            <Amount
+                                label="Gastado"
+                                amount={totalSpent}
+                            />
+                        </div>
+                    </div>
+
                     <h1 className='font-black text-4xl text-purple-950 mt-10'>
                         Gastos en este presupuesto
                     </h1>
@@ -56,7 +86,7 @@ export default async function BudgetDetailsPage({ params }: { params: Promise<{ 
                                     </div>
                                 </div>
 
-                                <ExpenseMenu 
+                                <ExpenseMenu
                                     expenseId={expense.id}
                                 />
 
